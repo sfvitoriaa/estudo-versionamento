291138eb37bd5e5c6429716528742fa568f4b18b
sfvitoriaa <0001046198@senaimgaluno.com.br>
2026-04-14 21:49:09 -0300

    4, 5

---
+++ src/pages/exercicio-5/index.tsx
@@ -0,0 +1,30 @@
+import {useEffect, useState} from 'react';
+
+export default function Ex05() {
+    const [users, setUsers] = useState<string[]>([]);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => setUsers(data.map((user: {name: string}) => user.name)))
+    }, []);
+
+    return(
+        <div className="p-8 flex flex-col gap-4">
+            <h1>Exercicio 05</h1>
+            <p>Aqui você vai fazer o Fetch de Usuários</p>
+            <ul>
+
+                {users.map((user, index) => (
+                    <li key={index}>{user}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+
+
+
+
+
