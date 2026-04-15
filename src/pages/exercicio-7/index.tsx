cbd6a764a987f4576c381be5ad4d97b0e987e560
sfvitoriaa <0001046198@senaimgaluno.com.br>
2026-04-14 22:08:18 -0300

    7

---
+++ src/pages/exercicio-7/index.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+export default function Ex07() {
+  const [task, setTask] = useState("");
+  const [list, setList] = useState<any[]>([]);
+
+  function addTask() {
+    if (task === "") return;
+
+    setList([...list, task]);
+    setTask("");
+  }
+
+  function removeTask(index: number) {
+    const newList = list.filter((_, i) => i !== index);
+    setList(newList);
+  }
+
+  return (
+    <div className="p-8 flex flex-col gap-4">
+      <h1>Exercício 07</h1>
+
+      <input
+        type="text"
+        placeholder="Digite uma tarefa"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+
+      <button onClick={addTask}>Adicionar</button>
+
+      {list.map((item, index) => (
+        <div key={index} style={{ display: "flex", gap: "10px" }}>
+          <p>{item}</p>
+          <button onClick={() => removeTask(index)}>Remover</button>
+        </div>
+      ))}
+    </div>
+  );
+}
